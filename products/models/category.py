d83066e083903dde929d5ad28c3104a64e395b93
Maxim Dobrinevsky <l7aBy4oK@gmail.com>
2023-12-09 16:47:18 +0300

    make unique field name for category models

--- products/models/category.py
+++ products/models/category.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class MainCategory(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     class Meta:
         db_table = "main_categories"
 
 
 class ChildCategory(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     parent_category = models.ForeignKey("MainCategory", on_delete=models.CASCADE)
 
     class Meta:
